9e94254c7d4e5ddd379b29d46dc051a5b9dd990e
Javier Vazquez <vzz_jav@yahoo.com>
2017-02-02 18:31:38 -0800

    IN PROGRESS!!!!

--- session_01/histogram.py
+++ session_01/histogram.py
@@ -4,17 +4,34 @@
 # Python 3.6
 
 from readExcel import excelOperations
-import os, pprint
+import os, math, statistics
 
 def main():
     excelOp = excelOperations()
     wb , ws = excelOp.readExcelFile("assigment_01.xlsx")
     print(wb.get_sheet_names())
     # excelOp.ws = wb.get_sheet_by_name("Data")
-    excelOp.displayContent()
+    # excelOp.displayContent()
     # excelOp.countByLabel()
-    list1, list2 = excelOp.excelColsToList()
-    excelOp.listToTextFile(list1)
+    female, male = excelOp.excelColsToList()
+    # excelOp.listToTextFile(list1)
+    print("Female population mean: ",statistics.mean(female))
+    print("Male population mean: ",statistics.mean(male))
+    print("Female population standard deviation: ",statistics.pstdev(female))
+    print("Male population standard deviation: ",statistics.pstdev(male))
+    print("Female population size: ",len(female))
+    print("Male population size: ",len(male))
+    mainList = female + male
+    print("Population size: ",len(mainList))
+    print("Maximum value: ", max(mainList))
+    print("Minimum value: ", min(mainList))
+    print("Female pop size: ",len(female))
+    print("Minimum female value: ", min(female))
+    print("Maximum female value: ", max(female))
+
+
+
+
 
 if __name__ == '__main__':
     print(os.path.basename(__file__))
