eb427b7ae0bea09850bf3bb4ae46af2cd9550c18
Javier Vazquez <vzz_jav@yahoo.com>
2017-02-28 03:28:40 -0800

    WORKS OK!!!

---
+++ session_04/assigment_04.py
@@ -0,0 +1,15 @@
+import read_MNIST_Class, os
+
+def main():
+    mnist = read_MNIST_Class.read_MNIST()
+    a = int(input("Negative Digit :"))
+    b = int(input("Positive Digit :"))
+    X = mnist.load_mnist('training', digits=[a,b])
+    print(X)
+    mnist.printPlot(X)
+
+
+
+if __name__ == '__main__':
+    print(os.path.basename(__file__))
+    main()
