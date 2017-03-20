7edaab1a710eadb435c8bad4479551ce7e7f4412
Javier Vazquez <vzz_jav@yahoo.com>
2017-03-19 21:41:59 -0700

    classifier

--- session_06/assigment_06.py
+++ session_06/assigment_06.py
@@ -48,38 +48,45 @@ def main():
     cols = "A:O"
     skip = 3
     Xtest = lc.loadData(file, sheet, cols, skip)
-    print(Xtest)
 
     print("\n\033[1mW - Binary Classifier\033[0m")
 
     # Tb for Binary Classifier
 
     fb = lc.W_XaT(Xtest, Wb)
-    print(fb)
-    print(fb.shape)
-
-    fbc = lc.classifier(fb)
+    fbc = lc.binaryClassClassifier(fb)
     print(fbc)
-    print(fbc.shape)
 
     # Tb for 6-Class Classifier
-
+    print("\n\033[1mW - 6-Class Classifier\033[0m")
     t6c = lc.W_XaT(Xtest, W6c)
-    print(Xtest)
-    print(W6c)
-    print(W6c.shape)
-    print(t6c)
-    print(t6c.shape)
-
     t6cb = lc.sixClassClassifier(t6c)
-    print(t6cb)
-    print(t6cb.shape)
-    #
-    # lc.invKesler(t6cb)
+    invKeslet = lc.invKesler(t6cb)
+    print(invKeslet)
 
-
+    # lc.dfToExcel(fbc, invKeslet, "classifiers.xlsx")
 
     # Testing Data - Ends
+
+    # Confusion Matrix Starts
+    # Binary Classifier Confusion Matrix (-1=Fail, +1=Pass)
+    print("\n\033[1mW - Confusion Binary Class Classifier\033[0m")
+    confusion2 = lc.W_XaT(Xtrain, Wb)
+    confusion2binary = lc.binaryClassClassifier(confusion2)
+
+    # lc.dfToExcel(Tb, confusion2binary, Tb==confusion2binary, "confusionBinary.xlsx")
+
+    tp, fn, tn, fp = lc.confusionBinaryMatrix(Tb, confusion2binary)
+
+    print("True Positive:", tp)
+    print("False Negative:", fn)
+    print("True Negative:", tn)
+    print("False Positive:", fp)
+    print("Sum:", np.sum([tp, fn, tn, fp]))
+    print("Accuracy:", (tp + tn) / (tp + fn + fp + tn))
+    print("Sensitivity:", tp / (tp + fn))
+    print("Specificity:", tn / (fp + tn))
+    print("PPV:", tp / (fp + tp))
 
 if __name__ == '__main__':
     print(os.path.basename(__file__))
--- session_06/linearClassifierClass.py
+++ session_06/linearClassifierClass.py
@@ -55,27 +55,47 @@ class linearClassifier():
         Txd[np.where(T[:,3]==1),0] = 3
         Txd[np.where(T[:,4]==1),0] = 4
         Txd[np.where(T[:,5]==1),0] = 5
-        print(np.hstack((T,Txd)))
-        # print(Txd)
-        # print(Txd.shape)
+        return Txd
 
-    def classifier(self, W_XaT):
+    def binaryClassClassifier(self, W_XaT):
         classifierArray = np.sign(W_XaT)
         return classifierArray
 
     def sixClassClassifier(self, W_XaT):
-        classifierArray = np.where(W_XaT == np.amax(W_XaT, axis=1))
+        m, n = W_XaT.shape
+        m = np.arange(m)
+        n = np.argmax(W_XaT, axis=1)
+        W_XaT[m,n] = 1
+        classifierArray = np.sign(W_XaT)
         return classifierArray
 
+    def confusionBinaryMatrix(self, original, generated):
+        table = np.hstack((original, generated))
+        np.set_printoptions(edgeitems=10)
+        tp = table[np.where((table == (1,1)).all(axis=1))]
+        fn = table[np.where((table == (1,-1)).all(axis=1))]
+        tn = table[np.where((table == (-1,-1)).all(axis=1))]
+        fp = table[np.where((table == (-1,1)).all(axis=1))]
+        tp, n = tp.shape
+        fn, n = fn.shape
+        tn, n = tn.shape
+        fp, n = fp.shape
+        return tp, fn, tn, fp
 
-    def dfToExcel(self, W, fileName):
+
+
+
+
+    def dfToExcel(self, var1, var2, var3, fileName):
         writer = pd.ExcelWriter(fileName)
-        W = pd.DataFrame(W)
-        W.to_excel(writer, "W")
+        var1 = pd.DataFrame(var1)
+        var2 = pd.DataFrame(var2)
+        var3 = pd.DataFrame(var3)
+        var1.to_excel(writer, "Tb")
+        var2.to_excel(writer, "Confusion")
+        var3.to_excel(writer, "True-False")
         writer.save()
         print("Variables to", fileName)
-
-
 
 if __name__ == '__main__':
     print("Direct access to " + os.path.basename(__file__))
